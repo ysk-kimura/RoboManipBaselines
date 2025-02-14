1d3bcd9ccc48920847a0f372e57487a369d2010c
ysk-kimura <kimura.ysk@aist.go.jp>
2025-02-14 13:04:02 +0900

    Override abstract methods in DummyRealEnv to test RealEnvBase.

--- robo_manip_baselines/tests/TestRealEnvBaseGetInfo.py
+++ robo_manip_baselines/tests/TestRealEnvBaseGetInfo.py
@@ -2,31 +2,54 @@ import unittest
 
 import cv2
 import numpy as np
-from envs.real.RealEnvGelsiteSample import RealEnvGelsiteSample
+
+from robo_manip_baselines.envs.real.RealEnvBase import RealEnvBase
 
 
-class TestRealEnvGelsiteSampleGetInfo(unittest.TestCase):
+class DummyRealEnv(RealEnvBase):
+    def __init__(self, gelsight_ids):
+        camera_ids = {}
+        super().__init__(
+            robot_ip=None, camera_ids=camera_ids, gelsight_ids=gelsight_ids
+        )
+        self.setup_realsense(camera_ids)
+        self.setup_gelsight(gelsight_ids)
+
+    def _reset_robot(self, *args, **kwargs):
+        pass
+
+    def _set_action(self, *args, **kwargs):
+        pass
+
+    def _get_obs(self, *args, **kwargs):
+        pass
+
+    @property
+    def tactile_names(self):
+        return self.tactiles.keys()
+
+
+class TestRealEnvBaseGetInfo(unittest.TestCase):
     def setUp(self):
-        self.real_env_gelsite_sample = RealEnvGelsiteSample()
-        self.camera_name = "tactile_left"
+        self.tactile_name = "tactile_left"
 
         # gsrobotics/examples/show3d.py
         #     the device ID can change after unplugging and changing the usb ports.
         #     on linux run, v4l2-ctl --list-devices, in the terminal to get the device ID for camera
-        camera_ids = {self.camera_name: "GelSight Mini R0B 2D16-V7R5: Ge"}
+        gelsight_ids = {self.tactile_name: "GelSight Mini R0B 2D16-V7R5: Ge"}
 
-        self.real_env_gelsite_sample.setup_gelsight(camera_ids)
+        self.dummy_real_env = DummyRealEnv(gelsight_ids=gelsight_ids)
 
-    def test_real_env_gelsite_sample_get_Info(self):
-        info = self.real_env_gelsite_sample._get_info()
-        rgb_image = info["rgb_images"][self.camera_name]
-        depth_image = info["depth_images"][self.camera_name]
+    def test_dummy_real_env_get_Info(self):
+        info = self.dummy_real_env._get_info()
+        rgb_image = info["rgb_images"][self.tactile_name]
+        depth_image = info["depth_images"][self.tactile_name]
 
         self.assertIsInstance(rgb_image, np.ndarray)
         self.assertEqual(rgb_image.dtype, np.uint8)
         self.assertEqual(rgb_image.shape[2], 3)  # 3 color channels
         self.assertIsNone(depth_image)
-        cv2.imshow("image", rgb_image)
+        cv2.imshow("rgb_image", rgb_image)
 
         print("Press any key to exit...")
         cv2.waitKey(0)

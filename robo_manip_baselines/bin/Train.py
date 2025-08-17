b64cfaf6e1a99f47dd2b7b9e9cc8ae0f2e85f04d
Masaki Murooka <m-murooka@aist.go.jp>
2025-08-17 22:11:48 +0900

    Add TrainMain class.

--- robo_manip_baselines/bin/Train.py
+++ robo_manip_baselines/bin/Train.py
@@ -3,53 +3,64 @@ import importlib
 import sys
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description="This is a meta argument parser for the train switching between different policies and environments. The actual arguments are handled by another internal argument parser.",
-        fromfile_prefix_chars="@",
-        add_help=False,
-    )
-    parser.add_argument(
-        "policy",
-        type=str,
-        nargs="?",
-        default=None,
-        choices=[
-            "Mlp",
-            "Sarnn",
-            "Act",
-            "MtAct",
-            "DiffusionPolicy",
-            "DiffusionPolicy3d",
-        ],
-        help="policy",
-    )
-    parser.add_argument(
-        "-h", "--help", action="store_true", help="Show this help message and continue"
-    )
+class TrainMain:
+    policy_parent_module_str = "robo_manip_baselines.policy"
 
-    args, remaining_argv = parser.parse_known_args()
-    sys.argv = [sys.argv[0]] + remaining_argv
-    if args.policy is None:
-        parser.print_help()
-        return
-    elif args.help:
-        parser.print_help()
-        print("\n================================\n")
-        sys.argv += ["--help"]
+    def __init__(self):
+        self.setup_args()
 
-    from robo_manip_baselines.common import camel_to_snake
+    def setup_args(self):
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description="This is a meta argument parser for the train switching between different policies and environments. The actual arguments are handled by another internal argument parser.",
+            fromfile_prefix_chars="@",
+            add_help=False,
+        )
+        parser.add_argument(
+            "policy",
+            type=str,
+            nargs="?",
+            default=None,
+            choices=[
+                "Mlp",
+                "Sarnn",
+                "Act",
+                "MtAct",
+                "DiffusionPolicy",
+                "DiffusionPolicy3d",
+            ],
+            help="policy",
+        )
+        parser.add_argument(
+            "-h",
+            "--help",
+            action="store_true",
+            help="Show this help message and continue",
+        )
 
-    policy_module = importlib.import_module(
-        f"robo_manip_baselines.policy.{camel_to_snake(args.policy)}"
-    )
-    TrainPolicyClass = getattr(policy_module, f"Train{args.policy}")
+        self.args, remaining_argv = parser.parse_known_args()
+        sys.argv = [sys.argv[0]] + remaining_argv
+        if self.args.policy is None:
+            parser.print_help()
+            sys.exit(1)
+        elif self.args.help:
+            parser.print_help()
+            print("\n================================\n")
+            sys.argv += ["--help"]
 
-    train = TrainPolicyClass()
-    train.run()
-    train.close()
+    def run(self):
+        from robo_manip_baselines.common import camel_to_snake
+
+        policy_module = importlib.import_module(
+            f"{self.policy_parent_module_str}.{camel_to_snake(self.args.policy)}"
+        )
+        TrainPolicyClass = getattr(policy_module, f"Train{self.args.policy}")
+
+        train = TrainPolicyClass()
+        train.run()
+        train.close()
 
 
 if __name__ == "__main__":
-    main()
+    main = TrainMain()
+    main.run()

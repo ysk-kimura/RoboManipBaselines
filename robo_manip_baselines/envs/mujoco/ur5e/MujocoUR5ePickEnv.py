89108c517988d5dedd8b14acf60fafadd0d54911
Masaki Murooka <m-murooka@aist.go.jp>
2025-08-22 14:08:39 +0900

    [envs] Allow to randomize object pos in MujocoUR5ePick.

--- robo_manip_baselines/envs/mujoco/ur5e/MujocoUR5ePickEnv.py
+++ robo_manip_baselines/envs/mujoco/ur5e/MujocoUR5ePickEnv.py
@@ -1,5 +1,6 @@
 from os import path
 
+import mujoco
 import numpy as np
 
 from .MujocoUR5eEnvBase import MujocoUR5eEnvBase
@@ -41,31 +42,54 @@ class MujocoUR5ePickEnv(MujocoUR5eEnvBase):
             **kwargs,
         )
 
-        # self.original_pole_pos = self.model.body("pole").pos.copy()
-        # self.pole_pos_offsets = np.array(
-        #     [
-        #         [0.0, 0.0, 0.0],
-        #         [0.0, 0.04, 0.0],
-        #         [0.0, 0.08, 0.0],
-        #         [0.0, 0.12, 0.0],
-        #         [0.0, 0.16, 0.0],
-        #         [0.0, 0.20, 0.0],
-        #     ]
-        # )  # [m]
+        self.obj_name_list = [f"obj{i}" for i in range(1, 10)]
+        self.original_obj_pos_list = [
+            self.model.body(obj_name).pos.copy() for obj_name in self.obj_name_list
+        ]
+        self.basket_name_list = [f"basket{i}" for i in range(1, 3)]
+        self.original_basket_pos_list = [
+            self.model.body(basket_name).pos.copy()
+            for basket_name in self.basket_name_list
+        ]
+        self.basket_pos_offsets = np.array(
+            [
+                [-0.09, 0.0, 0.0],
+                [-0.06, 0.0, 0.0],
+                [-0.03, 0.0, 0.0],
+                [0.0, 0.0, 0.0],
+                [0.03, 0.0, 0.0],
+                [0.06, 0.0, 0.0],
+            ]
+        )  # [m]
 
     # def _get_success(self):
     #     pass
 
     def modify_world(self, world_idx=None, cumulative_idx=None):
         if world_idx is None:
-            # world_idx = cumulative_idx % len(self.pole_pos_offsets)
-            world_idx = 0
+            world_idx = cumulative_idx % len(self.basket_pos_offsets)
 
-        # pole_pos = self.original_pole_pos + self.pole_pos_offsets[world_idx]
-        # if self.world_random_scale is not None:
-        #     pole_pos += np.random.uniform(
-        #         low=-1.0 * self.world_random_scale, high=self.world_random_scale, size=3
-        #     )
-        # self.model.body("pole").pos = pole_pos
+        basket_pos_offset = self.basket_pos_offsets[world_idx]
+        for basket_name, original_basket_pos in zip(
+            self.basket_name_list, self.original_basket_pos_list
+        ):
+            self.model.body(basket_name).pos = original_basket_pos + basket_pos_offset
+
+        if self.world_random_scale is not None:
+            for obj_name, original_obj_pos in zip(
+                self.obj_name_list, self.original_obj_pos_list
+            ):
+                obj_pos_offset = np.random.uniform(
+                    low=-1.0 * self.world_random_scale,
+                    high=self.world_random_scale,
+                    size=3,
+                )
+                obj_joint_id = mujoco.mj_name2id(
+                    self.model, mujoco.mjtObj.mjOBJ_JOINT, f"{obj_name}_freejoint"
+                )
+                obj_qpos_addr = self.model.jnt_qposadr[obj_joint_id]
+                self.init_qpos[obj_qpos_addr : obj_qpos_addr + 3] = (
+                    original_obj_pos + obj_pos_offset
+                )
 
         return world_idx

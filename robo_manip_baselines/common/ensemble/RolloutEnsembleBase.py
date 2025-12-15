292f41e662b4cb302e2a2fac91b106a17efe7644
ysk-kimura <kimura.ysk@aist.go.jp>
2025-12-15 13:44:17 +0900

    [common/ensemble] WIP: Add multi-instance run loop in RolloutEnsembleBase.py

--- robo_manip_baselines/common/ensemble/RolloutEnsembleBase.py
+++ robo_manip_baselines/common/ensemble/RolloutEnsembleBase.py
@@ -1,3 +1,6 @@
+import cv2
+
+
 class RolloutEnsembleBase:
     def __init__(self, rollout_instances=None):
         self.rollout_instances = rollout_instances or []
@@ -6,9 +9,40 @@ class RolloutEnsembleBase:
         self.rollout_instances = lst
 
     def reset(self, **kwargs):
-        for r in self.rollout_instances:
-            r.reset(**kwargs)
+        for rollout_instance in self.rollout_instances:
+            rollout_instance.reset(**kwargs)
 
     def run(self, **kwargs):
-        for r in self.rollout_instances:
-            r.run(**kwargs)
+        for rollout_instance in self.rollout_instances:
+            rollout_instance.reset_run_vars()
+
+            while True:
+                env_action = rollout_instance.get_env_action()
+                rollout_instance.record_rollout_data()
+
+                (
+                    rollout_instance.obs,
+                    rollout_instance.reward,
+                    _,
+                    _,
+                    rollout_instance.info,
+                ) = rollout_instance.env.step(env_action)
+
+                rollout_instance.post_phase_update()
+
+                rollout_instance.key = cv2.waitKey(1)
+                try:
+                    rollout_instance.check_phase_transition()
+                except AttributeError:
+                    pass  # Ignore AttributeError if phase_manager absent; safe as not all rollouts have it
+
+                if rollout_instance.key == 27:  # escape key
+                    rollout_instance.quit_flag = True
+                if rollout_instance.quit_flag:
+                    break
+
+            rollout_instance.dump_rollout_result()
+
+            rollout_instance.print_statistics()
+
+            # rollout_instance.env.close()

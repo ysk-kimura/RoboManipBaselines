b3dd4145c82e108f7806f3a578092a98c27f21dd
ysk-kimura <kimura.ysk@aist.go.jp>
2025-12-15 16:13:34 +0900

    [common/ensemble] WIP: Refactor RolloutEnsembleBase.py extract methods

--- robo_manip_baselines/common/ensemble/RolloutEnsembleBase.py
+++ robo_manip_baselines/common/ensemble/RolloutEnsembleBase.py
@@ -6,63 +6,85 @@ class RolloutEnsembleBase:
     def __init__(self, rollout_instances=None):
         self.rollout_instances = rollout_instances or []
 
-    # def set_rollout_inst_list(self, lst):
-    #     self.rollout_instances = lst
-
-    # def reset(self, **kwargs):
-    #     for rollout_instance in self.rollout_instances:
-    #         rollout_instance.reset(**kwargs)
-
-    def run(self, **kwargs):
+    def init_vars(self):
         for rollout_instance in self.rollout_instances:
             rollout_instance.reset_flag = True
             rollout_instance.quit_flag = False
             rollout_instance.inference_duration_list = []
 
-        while True:
-            env_action_parts = []
-            for rollout_instance in self.rollout_instances:
-                cmd_list = rollout_instance.fetch_env_commands()
-                if len(cmd_list) > 0:
-                    cmd_vec = np.concatenate(cmd_list)
-                else:
-                    cmd_vec = np.zeros(0, dtype=np.float64)
-                env_action_parts.append(cmd_vec)
-            env_action = np.mean(np.stack(env_action_parts, axis=0), axis=0).astype(
-                env_action_parts[0].dtype, copy=True
-            )
+    def fetch_env_commands(self, env_action_parts):
+        for rollout_instance in self.rollout_instances:
+            cmd_list = rollout_instance.fetch_env_commands()
+            if len(cmd_list) > 0:
+                cmd_vec = np.concatenate(cmd_list)
+            else:
+                cmd_vec = np.zeros(0, dtype=np.float64)
+            env_action_parts.append(cmd_vec)
 
-            for rollout_instance in self.rollout_instances:
-                if (
-                    rollout_instance.args.save_rollout
-                    and rollout_instance.phase_manager.is_phase("RolloutPhase")
-                ):
-                    rollout_instance.record_data()
+    def record_rollout_data(self):
+        for rollout_instance in self.rollout_instances:
+            if (
+                rollout_instance.args.save_rollout
+                and rollout_instance.phase_manager.is_phase("RolloutPhase")
+            ):
+                rollout_instance.record_data()
 
-            obs, reward, _, _, info = self.rollout_instances[0].env.step(env_action)
-            for rollout_instance in self.rollout_instances:
-                rollout_instance.obs = obs
-                rollout_instance.reward = reward
-                rollout_instance.info = info
+    def set_obs(self, obs, reward, info):
+        for rollout_instance in self.rollout_instances:
+            rollout_instance.obs = obs
+            rollout_instance.reward = reward
+            rollout_instance.info = info
 
-            for rollout_instance in self.rollout_instances:
-                rollout_instance.phase_manager.post_update()
+    def post_update(self):
+        for rollout_instance in self.rollout_instances:
+            rollout_instance.phase_manager.post_update()
 
-            for rollout_instance in self.rollout_instances:
-                rollout_instance.key = cv2.waitKey(1)
-                try:
-                    rollout_instance.phase_manager.check_transition()
-                except AttributeError:
-                    pass  # Ignore AttributeError if phase_manager absent; safe as not all rollouts have it
+    def check_transition(self):
+        for rollout_instance in self.rollout_instances:
+            try:
+                rollout_instance.phase_manager.check_transition()
+            except AttributeError:
+                pass  # Ignore AttributeError if phase_manager absent; safe as not all rollouts have it
 
-            if self.rollout_instances[0].key == 27:  # escape key
-                self.rollout_instances[0].quit_flag = True
-            if self.rollout_instances[0].quit_flag:
-                break
-
+    def save_rollout_results(self):
         for rollout_instance in self.rollout_instances:
             rollout_instance.dump_rollout_result()
 
             rollout_instance.print_statistics()
 
-        # self.rollout_instances[0].env.close()
+    def activate_quit_flag(self):
+        for rollout_instance in self.rollout_instances:
+            rollout_instance.quit_flag = True
+
+    def is_quit_activated(self):
+        for rollout_instance in self.rollout_instances:
+            if rollout_instance.quit_flag:
+                return True
+        return False
+
+    def run(self, **kwargs):
+        self.init_vars()
+
+        while True:
+            env_action_parts = []
+            self.fetch_env_commands(env_action_parts)
+            env_action = np.mean(np.stack(env_action_parts, axis=0), axis=0).astype(
+                env_action_parts[0].dtype, copy=True
+            )
+
+            self.record_rollout_data()
+
+            obs, reward, _, _, info = self.rollout_instances[0].env.step(env_action)
+            self.set_obs(obs, reward, info)
+
+            self.post_update()
+
+            self.key = cv2.waitKey(1)
+            self.check_transition()
+
+            if self.key == 27:  # escape key
+                self.activate_quit_flag()
+            if self.is_quit_activated():
+                break
+
+        self.save_rollout_results()
